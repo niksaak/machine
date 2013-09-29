665a067fdd40b3b83ae7d9283a7bc86c1715b1a1
Mikola Samardak <niksaak@gmail.com>
2013-09-29 14:39:29 +0300

    upgrade bullets
    + Add playerp parameter for player bullets
    + Use images from internal resources
    + Fix drawing coordinates
    + Check for bounds in updater
    + Add killer function
    
    Close #2

--- lua/bullet.lua
+++ lua/bullet.lua
@@ -2,30 +2,38 @@
 -- Bullet class
 -- version: 0.1
 -- date: 2013/09/29
--- author: roxy
+-- authors: roxy, niksaak
 ----------------------
 
 require('lua.lib.class')
+
+local images = {}
+images["yellow_bullet"] = love.graphics.newImage("gfx/yellow_bullet.png")
 
 ----------------------
 -- Define bullet class
 ----------------------
 Bullet = class(
-function(self, x, y, xforce, yforce, speed, image)
+function(self, x, y, xforce, yforce, speed, playerp, image)
   -- Init
   self.x = x
   self.y = y
   self.xforce = xforce
   self.yforce = yforce
-  self.speed =speed
+  self.speed = speed
   self.hb_r = 3
   -- Collide
   self.shape = Collider:addCircle(self.x, self.y, self.hb_r)
   self.shape.body = self
+  if (playerp) then
+    Collider:addToGroup('player', self.shape)
+  else
+    Collider:addToGroup('enemy', self.shape)
+  end
   -- Image
-  self.image = image
-  self.image_offx = image:getWidth()/2
-  self.image_offy = image:getHeight()/2
+  self.image = images[image]
+  self.image_offx = self.image:getWidth()/2
+  self.image_offy = self.image:getHeight()/2
 end
 )
 
@@ -34,7 +42,7 @@ end
 ----------------------
 function Bullet:draw()
   love.graphics.draw(self.image,
-                     self.x + Danmaku.x, self.y + Danmaku.y, -- pos
+                     self.x, self.y, -- pos
                      0, 1, 1, -- rotozoom
                      self.image_offx, self.image_offy) -- center
 end
@@ -43,6 +51,31 @@ end
 -- Update bullet
 ----------------------
 function Bullet:update(dt)
-  self.x = self.x + (self.xforce * self.speed * dt)
-  self.y = self.y + (self.yforce * self.speed * dt)
+  -- Compute coords
+  local newx = self.x + (self.xforce * self.speed * dt)
+  local newy = self.y + (self.yforce * self.speed * dt)
+  -- Set coords for bullet
+  self.x = newx
+  self.y = newy
+  -- Set coords for its' shape
+  self.shape:moveTo(newx, newy)
+
+  -- Kill bullet if it is out of boundaries
+  if (self.x < Danmaku.x or self.x > Danmaku.x + Danmaku.width or
+      self.y < Danmaku.y or self.y > Danmaku.y + Danmaku.height) then
+     self:die(true)
+   end
 end
+
+----------------------
+-- Kill bullet
+----------------------
+function Bullet:die(instantp)
+  Collider:remove(self.shape)
+  if (not instantp) then
+    -- TODO: cute bullet explosion here
+  end
+  self.x = -1
+  self.y = -1
+end
+
