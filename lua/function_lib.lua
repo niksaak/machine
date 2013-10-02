393aa9d530ea5000d3f96b7c17febd1a77539472
Mikola Samardak <niksaak@gmail.com>
2013-10-03 00:12:55 +0300

    (re)add some convenient lispy functions

--- lua/function_lib.lua
+++ lua/function_lib.lua
@@ -33,6 +33,61 @@ end
 function quit()
   love.event.push("quit")  
 end
+
+----------------------
+-- Lispy corner
+----------------------
+function complement(predicate)
+  -- take predicate and return function which mirrors it
+  return function(j)
+    if predicate(j) then
+      return false
+    else
+      return true
+    end
+  end
+end
+
+function some(test, tab)
+  -- return value of test as soon as test on any
+  -- element of seq returns non-false
+  for j in pairs(seq) do
+    local ret = test(j)
+    if ret then
+      return ret
+    end
+  end
+  return false
+end
+
+function notany(test, tab)
+  -- return false as soon as test on any element of seq returns non-false,
+  -- return true otherwise
+  return not some(test, seq)
+end
+
+function remove_if(test, tab)
+  -- make a copy of tab with pairs satisfying test removed
+  local ret = {}
+  for key, val in pairs(tab) do
+    if complement(test)(val) then
+      ret[key] = val
+    end
+  end
+  return ret
+end
+
+function remove_if_not(test, tab)
+  -- make a copy of tab with all pairs not satisfying test removed
+  local ret = {}
+  for key, val in pairs(tab) do
+    if test(val) then
+      ret[key] = val
+    end
+  end
+  return ret
+end
+
 ----------------------
 -- EOF
 ----------------------
