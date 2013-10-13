165406ed7bdfd6b3d53144bc77bca106eaf29f0b
Mikola Samardak <niksaak@gmail.com>
2013-10-13 16:30:04 +0300

    add means to iterate through events and link arrays of them
    Also, Event is now doubly linked, just in case.

--- lua/event.lua
+++ lua/event.lua
@@ -1,27 +1,68 @@
 Event = class(
-function(self, next)
+function(self, prev, next)
+  self.prev = prev
   self.next = next
 end)
 
 function Event:start()
+  -- called upon switching to this event
 end
 
 function Event:update()
+  -- called each game iteration when event is current
 end
 
 function Event:finish()
+  -- switch to next event or return
   if self.next == nil then
     return
   end
   self.next:start()
 end
 
-EventList = class(
+function eventlist(event)
+  -- returns iterator for eventlist
+  loval e = event
+  return function()
+    local re = e
+    e = e.next
+    return re
+  end
+end
+
+EventRunner = class(
 function(self, first)
-  self.list = first -- first event
+  self.list = first -- event list
 end)
 
-function EventList:run()
+function link_event_array(array)
+  -- link table members with each other ordered by position in array
+  local lst = nil
+  for e in ipairs(array) do
+    e.prev = lst
+    if e.prev then
+      e.prev.next = e
+    end
+  end
+  return lst
+end
+
+function EventRunner:addArray(array)
+  -- link the events in array and append them to the end of list
+  local lst = link_event_array(array)
+  if self.list then
+    for e in eventlist(self.list) do
+      if e.next == nil then
+        e.next = lst
+      end
+    end
+  else
+    self.list = lst
+  end
+end
+
+function EventRunner:run()
+  -- run eventlist
   assert(self.list)
   self.list:start()
 end
