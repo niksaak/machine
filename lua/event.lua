32e9b91917d21dd49ba680ed8837429d98ee17d2
Mikola Samardak <niksaak@gmail.com>
2013-10-14 00:49:55 +0300

    completely rewrite events to use closures

--- lua/event.lua
+++ lua/event.lua
@@ -1,69 +1,39 @@
-Event = class(
-function(self, prev, next)
-  self.prev = prev
-  self.next = next
-end)
-
-function Event:start()
-  -- called upon switching to this event
-end
-
-function Event:update()
-  -- called each game iteration when event is current
-end
-
-function Event:finish()
-  -- switch to next event or return
-  if self.next == nil then
-    return
-  end
-  self.next:start()
-end
-
-function eventlist(event)
-  -- returns iterator for eventlist
-  loval e = event
-  return function()
-    local re = e
-    e = e.next
-    return re
-  end
-end
+require 'lua.enemies.trans'
 
 EventRunner = class(
-function(self, first)
-  self.list = first -- event list
+function(self, array)
+  self.current = function() return true end -- starter, tee-hee
+  self.len = 0
+  self.num = 0
+  self.events = {}
+  if array ~= nil then
+    self:addArray(array)
+  end
 end)
 
-function link_event_array(array)
-  -- link table members with each other ordered by position in array
-  local lst = nil
-  for e in ipairs(array) do
-    e.prev = lst
-    if e.prev then
-      e.prev.next = e
-    end
-  end
-  return lst
+function EventRunner:addEvent(func)
+  print("add event", self.len)
+  self.len = self.len + 1
+  self.events[self.len] = func
 end
 
 function EventRunner:addArray(array)
   -- link the events in array and append them to the end of list
-  local lst = link_event_array(array)
-  if self.list then
-    for e in eventlist(self.list) do
-      if e.next == nil then
-        e.next = lst
-      end
-    end
-  else
-    self.list = lst
+  for _, v in ipairs(array) do
+    self:addEvent(v)
   end
 end
 
-function EventRunner:run()
-  -- run eventlist
-  assert(self.list)
-  self.list:start()
+function EventRunner:update()
+  if self.num > self.len then
+    return true
+  end
+  if self.current() then
+    print("event #", self.num, "done")
+    self.num = self.num + 1
+    local init = self.events[self.num]
+    self.current = init()
+  end
+  return false
 end
 
