b02b6332880330f21e622b7059b1e29b4225d7f1
Mikola Samardak <niksaak@gmail.com>
2013-10-14 23:39:07 +0300

    documentate entities

--- lua/entity.lua
+++ lua/entity.lua
@@ -5,29 +5,39 @@
 EntList = {}
 EntList.list = {}
 
+-- Put entity into the entlist.
 function EntList:put(entity)
   EntList.list[entity] = entity
 end
 
+-- Get entity from the entlist.
+-- FIXME: is there really a reason to have this considering that we use
+-- entity itself as a key?
 function EntList:get(entity)
   return EntList.list[entity]
 end
 
+-- Remove entity from the entlist.
 function EntList:remove(entity)
   EntList.list[entity] = nil
 end
 
+-- Clear the entlist.
 function EntList:clear()
   EntList.list = {}
 end
 
 
+-- Entity primitive carries its' global position and has an entry in EntList.
 Entity = class(
 function(self, x, y)
   self.x = x + Danmaku.x
   self.y = y + Danmaku.y
   EntList:put(self)
 end)
+
+-- The four methods following return and set position relative to Danmaku
+-- topleft corner. To work with global position use the x and y fields.
 
 function Entity:getx()
   return self.x - Danmaku.x
@@ -44,6 +54,8 @@ end
 function Entity:sety(n)
   self.y = n + Danmaku.y
 end
+
+-- Each entity shall be able to update and draw itself.
 
 function Entity:update(dt)
 end
