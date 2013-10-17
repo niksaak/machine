f46a3bb2d1df03ab5a98aafe263a2b9b4bfadcaa
Roxugen <agroxy@yandex.ru>
2013-10-17 16:56:48 +0700

    Add control keys map

--- lua/define.lua
+++ lua/define.lua
@@ -44,12 +44,12 @@ is_hard = false
 ----------------------
 -- Key shortcuts
 ----------------------
---[[key = {
-  action      = {'z', 'enter', 'space'}
-  bomb        = {'x'}
-  focus       = {'shift'}
+control_key = {
+  action      = {'z', 'return'},
+  bomb        = {'x'},
+  focus       = {'shift'},
   escape      = {'esc'}
-}]]
+}
 
 ----------------------
 -- EOF
