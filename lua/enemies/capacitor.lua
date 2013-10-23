4960d784ddbccb97050e5322f902db3e8eb99411
Mikola Samardak <niksaak@gmail.com>
2013-10-23 18:43:45 +0300

    rewrite capacitor enemy to use ticker-tocker
    XXX: does fail with 'attempt to index local 'self' (a nil value)'.

--- lua/enemies/capacitor.lua
+++ lua/enemies/capacitor.lua
@@ -1,20 +1,26 @@
 require('lua.enemy')
+require('lua.timer')
 
 Capacitor = class(Enemy,
 function(self, x, y, speed)
   Enemy.init(self,x, y, 0, 1, speed, 'capacitor')
-  self.t = Timer.add(1.2, function() self:shoot() end)
+  self.tmr = Timer(1.2)
   self.lives = 10
 end)
 
-function Capacitor:shoot()
-  for i=0,4 do
-    Bullet(self:getx(), self:gety(), 0, 1 + i/10, 80, false, 'yellow')
+function Capacitor:update(dt)
+  Enemy.update(self, dt)
+  if self.tmr:tick(dt) then
+    self.shoot()
   end
-  self.t = Timer.add(1.2, function() self:shoot() end)
 end
 
-function Capacitor:afterdeath()
-  Timer.cancel(self.t)
+function Capacitor:shoot()
+  for i=0,4 do
+    -- XXX: attempt to index local 'self' (a nil value)
+    -- Probably getx() and gety() methods are unable to get capacitors' self
+    -- for some reason.
+    Bullet(self:getx(), self:gety(), 0, 1 + i/10, 80, false, 'yellow')
+  end
 end
 
