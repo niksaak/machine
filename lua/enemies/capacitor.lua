9c154ac7e6850ad53858c05747689923652207aa
Roxugen <agroxy@yandex.ru>
2013-10-26 12:33:52 +0700

    nerf capacitor
    Change hitbox radius to 10 (10+10 = capacitor width).
    Slower (?) his shot power.

--- lua/enemies/capacitor.lua
+++ lua/enemies/capacitor.lua
@@ -3,7 +3,7 @@ require('lua.timer')
 
 Capacitor = class(Enemy,
 function(self, x, y, speed)
-  Enemy.init(self,x, y, 0, 1, speed, 'capacitor')
+  Enemy.init(self,x, y, 0, 1, speed, 10, 'capacitor')
   self.tmr = Timer(1.2)
   self.lives = 10
 end)
@@ -16,8 +16,8 @@ function Capacitor:update(dt)
 end
 
 function Capacitor:shoot()
-  for i=0,4 do
-    Bullet(self:getx(), self:gety(), 0, 1 + i/10, 80, false, 'yellow')
+  for i=0,1 do
+    Bullet(self:getx(), self:gety() + i*10, 0, 1, 80, false, 'yellow')
   end
 end
 
